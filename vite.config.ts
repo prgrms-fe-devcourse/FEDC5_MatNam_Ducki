f75e023302fb91fbaa9e2ecfd682c472db9ddc15
pySoo <soopy368@gmail.com>
2023-12-27 10:26:02 +0900

    [✨feat]: alias 절대 경로 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import * as path from 'path';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 3000,
+  },
   plugins: [react()],
-})
+});
