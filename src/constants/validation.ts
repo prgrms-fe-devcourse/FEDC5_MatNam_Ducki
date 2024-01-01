759f4ee8794556e7f16441055a6e53aece37707c
pySoo <soopy368@gmail.com>
2024-01-01 15:30:35 +0900

    [✨feat]: Input Validation 상수 선언

---
+++ src/constants/validation.ts
@@ -0,0 +1,19 @@
+export const INPUT_VALIDATION = {
+  EMAIL: {
+    pattern: {
+      value:
+        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+      message: '이메일 형식과 맞지 않습니다.',
+    },
+  },
+  PASSWORD: {
+    minLength: {
+      value: 8,
+      message: '최소 8자리 이상 입력해 주세요.',
+    },
+    pattern: {
+      value: /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[a-z\d!@#$%^&*]{8,}$/i,
+      message: '영어 소문자, 숫자, 특수문자를 각각 1개 이상 포함해야 합니다.',
+    },
+  },
+};
