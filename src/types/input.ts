76aaa333a2570bea70d3865545cf2a1437b49913
pySoo <soopy368@gmail.com>
2024-01-01 11:57:11 +0900

    [✨feat]: HookFormInput type 정의

---
+++ src/types/input.ts
@@ -0,0 +1,17 @@
+import {
+  FieldErrors,
+  FieldValues,
+  Path,
+  UseFormRegister,
+} from 'react-hook-form';
+
+export interface HookFormInputProps<T extends FieldValues>
+  extends React.ComponentProps<'input'> {
+  label: string;
+  name: Path<T>;
+  register: UseFormRegister<T>;
+  required: boolean;
+  errors?: FieldErrors<T>;
+  errorMessage?: string;
+  type?: 'email' | 'password' | 'text';
+}
