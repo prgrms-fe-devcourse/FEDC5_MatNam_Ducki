6a5a4fe89d5cd461026189e14c76a2c9f65f2a3a
pySoo <soopy368@gmail.com>
2024-01-02 12:24:25 +0900

    [✨feat]: endPoint 상수 선언

---
+++ src/services/endPoint.ts
@@ -0,0 +1,59 @@
+export const ENDPOINT = {
+  /* auth */
+  SIGNIN: '/login',
+  SIGNUP: '/signUp',
+  SIGNOUT: '/logout',
+  AUTH_USER: '/auth-user',
+
+  /* channel */
+  CHANNELS: {
+    ALL: '/channels',
+    CHANNEL: (channelName: string) => `/channels/${channelName}`,
+    CREATE: '/channels/create',
+  },
+
+  /* posts */
+  POSTS: {
+    CREATE: '/posts/create',
+    UPDATE: '/posts/update',
+    DELETE: '/posts/delete',
+    POST: (postId: string) => `/posts/${postId}`,
+    BY_CHANNEL_ID: (channelId: string) => `/posts/channel/${channelId}`,
+    BY_USER_ID: (userId: string) => `/posts/author/${userId}`,
+  },
+
+  LIKES: {
+    CREATE: '/likes/create',
+    DELETE: '/likes/delete',
+  },
+
+  COMMENTS: {
+    CREATE: '/comments/create',
+    DELETE: '/comments/delete',
+  },
+
+  /* user */
+  USERS: {
+    GET_USERS: '/users/get-users',
+    USER: (userId: string) => `/users/${userId}`,
+    UPLOAD_PHOTO: '/users/upload-photo',
+  },
+
+  SETTINGS: {
+    UPDATE_USER: '/settings/update-user',
+    UPDATE_PASSWORD: '/settings/update-password',
+  },
+
+  /* search */
+  SEARCH: {
+    USERS: (query: string) => `/search/users/${query}`,
+    ALL: (query: string) => `/search/all/${query}`,
+  },
+
+  /* notification */
+  NOTIFICATIONS: {
+    ALL: '/notifications',
+    SEEN: '/notifications/seen',
+    CREATE: '/notifications/create',
+  },
+};
