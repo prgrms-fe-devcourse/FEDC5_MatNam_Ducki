2d2bf2c8e18cf2a78526eff0f4f42b1f6f5b6d2b
pySoo <soopy368@gmail.com>
2024-01-02 12:29:15 +0900

    [✨feat]: 유저 관련 API 함수 추가

---
+++ src/services/User/user.ts
@@ -0,0 +1,53 @@
+import { User } from '@/types';
+import { GetUsersPayload, UpdateProfileImagePayload } from '@/types/payload';
+
+import { axiosInstance } from '../axiosInstance';
+import { ENDPOINT } from '../endPoint';
+
+export const getUsers = async ({ offset = 0, limit = 10 }: GetUsersPayload) => {
+  try {
+    const response = await axiosInstance.get<User[]>(ENDPOINT.USERS.GET_USERS, {
+      params: {
+        offset,
+        limit,
+      },
+    });
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const getUser = async (userId: string) => {
+  try {
+    const response = await axiosInstance.get<User>(ENDPOINT.USERS.USER(userId));
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+export const updateProfileImage = async ({
+  image,
+  isCover = false,
+}: UpdateProfileImagePayload) => {
+  try {
+    const formData = new FormData();
+    formData.append('isCover', isCover ? 'true' : 'false');
+    formData.append('image', image);
+
+    const response = await axiosInstance.post<User>(
+      ENDPOINT.USERS.UPLOAD_PHOTO,
+      formData,
+    );
+
+    return response;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
