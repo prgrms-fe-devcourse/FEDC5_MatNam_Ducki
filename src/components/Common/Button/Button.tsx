63247adcc6d01a71bc3a0a4ada02253d79f157ba
Baek <woonn97@gmail.com>
2024-01-02 20:12:39 +0900

    [✨feat]: Button 공통 컴포넌트 추가

---
+++ src/components/Common/Button/Button.tsx
@@ -0,0 +1,48 @@
+import styled from '@emotion/styled';
+import React from 'react';
+
+interface ButtonProps {
+  width?: string;
+  height?: string;
+  borderRadius?: string;
+  backgroundColor?: string;
+  textSize?: string;
+  textColor?: string;
+  children: React.ReactNode;
+}
+
+const CommonButton = styled.button<ButtonProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  border-radius: ${(props) => props.borderRadius};
+  background-color: ${(props) => props.backgroundColor};
+  font-size: ${(props) => props.textSize};
+  color: ${(props) => props.textColor};
+  cursor: pointer;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+export default function Button({
+  width,
+  height,
+  borderRadius,
+  backgroundColor,
+  textSize,
+  textColor,
+  children,
+  ...props
+}: ButtonProps) {
+  return (
+    <CommonButton
+      width={width}
+      height={height}
+      borderRadius={borderRadius}
+      backgroundColor={backgroundColor}
+      textSize={textSize}
+      textColor={textColor}
+      {...props}>
+      {children}
+    </CommonButton>
+  );
+}
