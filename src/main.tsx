9a9dedcdcd24696b3ddb1e151a9ab557f0edd585
nimgnas <sangminyn@gmail.com>
2024-01-02 11:49:48 +0900

    [🎨style]: emotion 전역 스타일 적용, 타입 정의

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import './index.css';
 
+import { ThemeProvider } from '@emotion/react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
+import { theme } from './styles/Theme.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 );
