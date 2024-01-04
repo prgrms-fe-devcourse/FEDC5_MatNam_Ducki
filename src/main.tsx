f377b741138f253a6f3219bb077c75ac460b2839
Baek <woonn97@gmail.com>
2024-01-04 10:46:26 +0900

    [✨feat]: react-query 초기 설정

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,25 @@
 import './index.css';
 
 import { ThemeProvider } from '@emotion/react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App.tsx';
 import { theme } from './styles/Theme.ts';
 
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: { retry: 1 },
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
     </ThemeProvider>
   </React.StrictMode>,
 );
