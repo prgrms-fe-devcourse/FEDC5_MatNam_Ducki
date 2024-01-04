a764a13f72f9fc1fef1c94ac00dda7cc5e1747c7
Kyungbin <pkb8839@naver.com>
2024-01-04 16:45:24 +0900

    [♻️refactor]: UserInfo 네이밍 변경

--- src/components/UserInfo/UserInfo.tsx
+++ src/components/UserInfo/UserInfo.tsx
@@ -1,3 +1,25 @@
-export default function UserInfo() {
-  return <div></div>;
+import styled from '@emotion/styled';
+
+import { PropsUserInfo } from '@/types/profile';
+
+const UserInfoSection = styled.div`
+  margin-top: 20px;
+`;
+
+const UserName = styled.div`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
+const UserId = styled.div`
+  color: #777777;
+`;
+
+export default function UserInfo({ userName, userId }: PropsUserInfo) {
+  return (
+    <UserInfoSection>
+      <UserName>{userName}</UserName>
+      <UserId>@{userId}</UserId>
+    </UserInfoSection>
+  );
 }
