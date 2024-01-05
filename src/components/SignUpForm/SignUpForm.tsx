5f3b2bf5b4e14f90d6b44d834227fc6838422670
pySoo <soopy368@gmail.com>
2024-01-05 13:55:37 +0900

    [✨feat]: 비밀번호 실시간 유효성 검사 로직 추가

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -7,6 +7,10 @@ import { ACCESS_TOKEN_KEY } from '@/constants/api';
 import { INPUT_VALIDATION } from '@/constants/validation';
 import { signUp } from '@/services/Auth/auth';
 import { HookFormInputListProps } from '@/types/input';
+import {
+  isPasswordContainsSpecialChar,
+  isPasswordShort,
+} from '@/utils/validation';
 
 import HookFormInput from './Common/HookFormInput';
 
@@ -32,10 +36,16 @@ const SignupButton = styled.button<{ isValid: boolean }>`
 `;
 
 export default function SignupForm() {
+  const GuideWrapper = styled.div`
+    display: block;
+    padding-top: 1rem;
+  `;
+
   const {
     register,
     handleSubmit,
     getValues,
+    watch,
     formState: { errors, isValid },
   } = useForm<SignupValues>();
 
@@ -63,7 +73,12 @@ export default function SignupForm() {
     mutation.mutate(signUpInput);
   };
 
-  const signupInputList: HookFormInputListProps<SignupValues> = [
+  const password = watch('password');
+
+  const isPasswordValidated =
+    !isPasswordShort(password) && isPasswordContainsSpecialChar(password);
+
+  const inputList: HookFormInputListProps<SignupValues> = [
     {
       label: '이름',
       name: 'fullName',
@@ -86,6 +101,16 @@ export default function SignupForm() {
       placeholder: '비밀번호를 입력해 주세요.',
       type: 'password',
       validation: INPUT_VALIDATION.PASSWORD,
+      guide: !isPasswordValidated && (
+        <GuideWrapper>
+          {isPasswordShort(password) && (
+            <p> · 8글자 이상으로만 입력할 수 있습니다.</p>
+          )}
+          {!isPasswordContainsSpecialChar(password) && (
+            <p>· 특수 문자를 반드시 포함해야합니다.</p>
+          )}
+        </GuideWrapper>
+      ),
     },
     {
       label: '비밀번호 확인',
