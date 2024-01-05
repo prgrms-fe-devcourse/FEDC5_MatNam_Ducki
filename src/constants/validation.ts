ca4e99f0661f6a5c32d3d03a912d9eab602369e1
pySoo <soopy368@gmail.com>
2024-01-05 14:02:19 +0900

    [✨feat]: 이메일, 비밀번호 정규식 상수로 분리

--- src/constants/validation.ts
+++ src/constants/validation.ts
@@ -1,3 +1,9 @@
+export const REGEX = {
+  EMAIL:
+    /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+  PASSWORD: /.*[!@#$%^&*()_+{}[\]:;"'<>,.?/].*/,
+};
+
 export const INPUT_VALIDATION = {
   FULLNAME: {
     minLength: {
@@ -7,8 +13,7 @@ export const INPUT_VALIDATION = {
   },
   EMAIL: {
     pattern: {
-      value:
-        /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+      value: REGEX.EMAIL,
       message: '이메일 형식과 맞지 않습니다.',
     },
   },
@@ -18,8 +23,8 @@ export const INPUT_VALIDATION = {
       message: '최소 8자리 이상 입력해 주세요.',
     },
     pattern: {
-      value: /^(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*])[a-z\d!@#$%^&*]{8,}$/i,
-      message: '영어 소문자, 숫자, 특수문자를 각각 1개 이상 포함해야 합니다.',
+      value: REGEX.PASSWORD,
+      message: '특수 문자를 1개 이상 입력해 주세요.',
     },
   },
 };
