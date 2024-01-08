3c202c7262d6235315a918be2b18bd4a792cc62e
pySoo <soopy368@gmail.com>
2024-01-08 12:01:06 +0900

    [✨feat]: signOut hook 추가

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,7 +1,7 @@
 import { useMutation } from '@tanstack/react-query';
 
 import { ACCESS_TOKEN_KEY } from '@/constants/api';
-import { signIn, signUp } from '@/services/Auth/auth';
+import { signIn, signOut, signUp } from '@/services/Auth/auth';
 
 interface AuthProps {
   onSuccess?: () => void;
@@ -40,6 +40,23 @@ export const useSignIn = ({ onSuccess }: AuthProps = {}) => {
         if (onSuccess) {
           onSuccess();
         }
+      }
+    },
+  });
+};
+
+/**
+ * @description useMutation으로 로그아웃 API를 호출하는 hook입니다. 성공 시 access token을 로컬 스토리지에서 삭제합니다.
+ * @param onSuccess - optional) 성공 시 수행할 callback 함수를 넘겨줄 때 사용합니다.
+ */
+export const useSignOut = ({ onSuccess }: AuthProps = {}) => {
+  return useMutation({
+    mutationFn: signOut,
+    onSuccess: () => {
+      localStorage.removeItem(ACCESS_TOKEN_KEY);
+
+      if (onSuccess) {
+        onSuccess();
       }
     },
   });
