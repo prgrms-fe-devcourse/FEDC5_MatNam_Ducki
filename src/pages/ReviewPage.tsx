9d65f5fa032083e3a2daaebf5079322760701102
Baek <woonn97@gmail.com>
2024-01-08 09:52:39 +0900

    [♻️refactor]: ImageUpload 컴포넌트로 변경

--- src/pages/ReviewPage.tsx
+++ src/pages/ReviewPage.tsx
@@ -1,43 +1,16 @@
 import styled from '@emotion/styled';
-import React, { useRef, useState } from 'react';
+import { useState } from 'react';
+
+import ImageUpload from '@/components/Common/ImageUpload';
 
 export default function ReviewPage() {
-  const [image, setImage] = useState<string | null>(null);
   const [file, setFile] = useState<File | null>(null);
-  const imageInputRef = useRef<HTMLInputElement>(null);
-  const elementRef = useRef<HTMLDivElement>(null);
 
-  const createURL = (selectedFile: File | null) => {
-    if (!selectedFile) {
-      return '';
-    }
+  const image = file ? URL.createObjectURL(file) : null; // 파일이 있으면 url을 만들어서 image에 넣어줍니다.
 
-    return URL.createObjectURL(selectedFile);
+  const handleFileChange = (file: File | null) => {
+    setFile(file); // 파일이 선택되면 file state를 업데이트
   };
-
-  const handleImageFilesChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    if (!event.target.files) {
-      return;
-    }
-
-    setImage(createURL(event.target.files[0]));
-    setFile(event.target.files[0]);
-  };
-
-  const handleImageInputClick = () => {
-    if (!imageInputRef.current) {
-      return;
-    }
-    imageInputRef.current.click();
-    console.log('imageInputRef.current', imageInputRef.current);
-  };
-
-  const handleImageRemove = () => {
-    setImage(null);
-  };
-
   return (
     <>
       <ReviewContainer>
@@ -47,30 +20,7 @@ export default function ReviewPage() {
           </section>
           <section className="relative">
             <TitleInput placeholder="제목을 입력해주세요." />
-            <Section>
-              <ImamgeInput
-                ref={imageInputRef}
-                onChange={handleImageFilesChange}
-                type="file"
-                accept="image/*"
-              />
-              {image ? (
-                <div ref={elementRef} className="w-full">
-                  <ImageContainer>
-                    <Image src={image} alt="이미지 미리보기" />
-                    <ImageButton onClick={handleImageRemove} type="button">
-                      이미지 삭제
-                    </ImageButton>
-                  </ImageContainer>
-                </div>
-              ) : (
-                <AddImage onClick={handleImageInputClick}>
-                  <span className="text-[0.875rem] text-gray-400">
-                    이미지 추가
-                  </span>
-                </AddImage>
-              )}
-            </Section>
+            <ImageUpload image={image} onFileChange={handleFileChange} />
             <ReviewTextArea name="content" placeholder="내용을 작성해보세요." />
             <CreateButton>등록</CreateButton>
           </section>
@@ -91,54 +41,6 @@ const TitleInput = styled.input`
   border-width: 0.5px; /* border-0.5 */
   border-color: #4b5563; /* border-gray-600 */
   margin-bottom: 1.25rem; /* mb-5 */
-`;
-
-const ImamgeInput = styled.input`
-  display: none;
-  border: 1.5px solid gray;
-`;
-
-const ImageContainer = styled.div`
-  position: relative;
-  aspect-ratio: 5 / 3;
-  width: 100%;
-  flex-shrink: 0;
-  overflow: hidden;
-  border-radius: 1rem;
-  border: 1.5px solid gray;
-  background: white;
-`;
-
-const Image = styled.img`
-  aspect-ratio: 5 / 3;
-  width: 100%;
-  object-fit: cover;
-`;
-
-const ImageButton = styled.button`
-  position: absolute;
-  bottom: 1px;
-  right: 1px;
-  height: 12px;
-  width: 12px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  background: white;
-`;
-
-const AddImage = styled.div`
-  display: flex;
-  aspect-ratio: 5 / 3;
-  width: 100%;
-  flex-shrink: 0;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  border-radius: 0.3125rem;
-  border: 1.5px solid gray;
-  background: white;
 `;
 
 const ReviewTextArea = styled.textarea`
