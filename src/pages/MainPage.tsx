9cd82284abaef2cbf4f0f502366c36482074221e
pySoo <soopy368@gmail.com>
2024-01-09 12:15:18 +0900

    [✨feat]: ErrorBoundary, Suspense 적용, 전역 컴포넌트 선언

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,3 +1,21 @@
+import { Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
+import { Outlet } from 'react-router-dom';
+
+import BottomNavBar from '@/components/BottomNavBar/BottomNavBar';
+import ErrorFallback from '@/components/Common/ErrorFallback';
+import ModalContainer from '@/components/Common/Modal/ModalContainer';
+
 export default function MainPage() {
-  return <></>;
+  return (
+    <>
+      <ErrorBoundary FallbackComponent={ErrorFallback}>
+        <Suspense>
+          <Outlet />
+          <ModalContainer />
+        </Suspense>
+      </ErrorBoundary>
+      <BottomNavBar />
+    </>
+  );
 }
