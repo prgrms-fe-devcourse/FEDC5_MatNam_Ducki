d593dfdcbf7922d3831d1b53b8f5d9e7aeffde4c
nimgnas <sangminyn@gmail.com>
2024-01-11 21:57:59 +0900

    [✨feat: queryKey 변경,  useLike hook 추가

--- src/hooks/ReviewDetail/index.ts
+++ src/hooks/ReviewDetail/index.ts
@@ -1,29 +1,40 @@
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 
 import { createComment } from '@/services/Post/comment';
+import { createLike, deleteLike } from '@/services/Post/like';
 import { getPostDetail } from '@/services/Post/post';
 
 const detailPageKey = {
-  getDetail: ['post'] as const,
+  post: (postId: string) => ['post', postId] as const,
 };
 
 export const useGetDetail = ({ postId }: { postId: string }) => {
   return useQuery({
-    queryKey: detailPageKey.getDetail,
+    queryKey: detailPageKey.post(postId),
     queryFn: () => getPostDetail(postId),
+    suspense: true,
   });
 };
 
-export const useCreateComment = () => {
+export const useCreateComment = ({ postId }: { postId: string }) => {
   const queryClient = useQueryClient();
 
   return useMutation({
-    mutationKey: detailPageKey.getDetail,
     mutationFn: createComment,
     onSuccess() {
-      queryClient.invalidateQueries({ queryKey: ['post'] });
+      queryClient.invalidateQueries({ queryKey: detailPageKey.post(postId) });
     },
   });
 };
 
-export const useLike = () => {};
+export const useLike = ({
+  postId,
+  isLike,
+}: {
+  postId: string;
+  isLike: boolean;
+}) => {
+  return useMutation({
+    mutationFn: isLike ? () => createLike(postId) : () => deleteLike(postId),
+  });
+};
