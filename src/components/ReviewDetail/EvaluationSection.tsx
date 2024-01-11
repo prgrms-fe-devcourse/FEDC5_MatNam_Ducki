d506e64d20e3449417b92cc2f45db0719b78bb3a
nimgnas <sangminyn@gmail.com>
2024-01-11 22:01:42 +0900

    [✨feat]: 좋아요 기능 추가, 디바운싱 적용

--- src/components/ReviewDetail/EvaluationSection.tsx
+++ src/components/ReviewDetail/EvaluationSection.tsx
@@ -1,5 +1,10 @@
+import { css } from '@emotion/react';
 import styled from '@emotion/styled';
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import { useGetDetail, useLike } from '@/hooks/ReviewDetail';
+import { useCheckAuthUser } from '@/hooks/useAuth';
 import { theme } from '@/styles/Theme';
 
 const StyledEvaluationSection = styled.div`
@@ -18,39 +23,67 @@ const LikeText = styled.span`
   font-weight: ${theme.fontWeight.bold};
 `;
 
-const HateText = styled.span`
-  color: ${theme.colors.secondary};
-  font-weight: ${theme.fontWeight.bold};
-`;
-
 const EvaluationRightWrapper = styled.div`
   display: flex;
   gap: 0.625rem;
 `;
 
-const LikeBtn = styled.button`
+const LikeBtn = styled.button<{ isLike: boolean }>`
   border: 1px solid ${theme.colors.accent};
   padding: 0.1875rem 0.8125rem;
   border-radius: 8px;
-`;
 
-const HateBtn = styled.button`
-  border: 1px solid ${theme.colors.secondary};
-  padding: 0.1875rem 0.8125rem;
-  border-radius: 8px;
+  ${(props) =>
+    props.isLike &&
+    css`
+      background-color: ${theme.colors.accent};
+    `}
 `;
 
 export default function EvaluationSection() {
-  return (
-    <StyledEvaluationSection>
-      <EvaluationLeftWrapper>
-        <LikeText>좋아요 3</LikeText>
-        <HateText>싫어요 0</HateText>
-      </EvaluationLeftWrapper>
-      <EvaluationRightWrapper>
-        <LikeBtn>👍</LikeBtn>
-        <HateBtn>👎</HateBtn>
-      </EvaluationRightWrapper>
-    </StyledEvaluationSection>
-  );
+  const { postId } = useParams() as { postId: string };
+  const { data: postData } = useGetDetail({ postId });
+  const { data: userData } = useCheckAuthUser();
+
+  const [isLike, setIsLike] = useState(false);
+  const [likeCount, setLikeCount] = useState(postData!.likes.length);
+  const [timer, setTimer] = useState(0);
+
+  const { mutate } = useLike({ postId, isLike });
+
+  //최초 렌더링시 좋아요 체크
+  useEffect(() => {
+    setIsLike(postData?.likes.some((like) => like?.user === userData?._id)!);
+  }, [postData, userData]);
+
+  const handleLikeClick = () => {
+    setIsLike((prevState) => !prevState);
+
+    isLike
+      ? setLikeCount((prev) => prev - 1)
+      : setLikeCount((prev) => prev + 1);
+
+    if (timer) clearTimeout(timer);
+    const newTimer = window.setTimeout(() => {
+      postData?.likes.some((like) => like?.user === userData?._id) === isLike &&
+        mutate();
+    }, 500);
+
+    setTimer(newTimer);
+  };
+
+  if (postData && userData) {
+    return (
+      <StyledEvaluationSection>
+        <EvaluationLeftWrapper>
+          <LikeText>좋아요 {likeCount}</LikeText>
+        </EvaluationLeftWrapper>
+        <EvaluationRightWrapper>
+          <LikeBtn isLike={isLike} onClick={handleLikeClick}>
+            👍
+          </LikeBtn>
+        </EvaluationRightWrapper>
+      </StyledEvaluationSection>
+    );
+  }
 }
