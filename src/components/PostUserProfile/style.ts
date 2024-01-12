2f4c0f7473c825e3719b9a2a134abc9bb31b74ee
Kyungbin <pkb8839@naver.com>
2024-01-12 14:33:59 +0900

    [✨feat] : 작성자 유저의 프로필페이지

---
+++ src/components/PostUserProfile/style.ts
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled';
+
+export const ProfileWrapper = styled.div`
+  width: 37.5rem;
+  height: 81.2rem;
+  background-color: #ffffff;
+`;
+
+export const ProfileTopWrapper = styled.div`
+  width: 100%;
+  height: 26rem;
+`;
+
+export const ProfileBackGroundImage = styled.div`
+  width: 37.5rem;
+  height: 23.2rem;
+  background-image: url('../../../public/Group.png');
+  background-size: cover;
+`;
+
+export const UserInfoWrapper = styled.div`
+  display: flex;
+  padding-top: 16rem;
+  width: 100%;
+`;
+
+export const UserWrapper = styled.div`
+  display: flex;
+  height: 30rem;
+  width: 33.5rem;
+  gap: 1rem;
+  flex-direction: column;
+  margin: 3rem 2rem 0 2rem;
+`;
+
+export const ImageWrapper = styled.div`
+  margin-top: 1.2rem;
+  margin-left: 4rem;
+`;
+
+export const Label = styled.span`
+  color: #868686;
+  font-size: 1.3rem;
+`;
+
+export const IntroductionWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 1rem;
+`;
+
+export const Introduction = styled.span`
+  margin-top: 0.6rem;
+  line-height: 2.08rem;
+  width: 33.5rem;
+  box-sizing: border-box;
+`;
+
+export const IntroductionBar = styled.div`
+  width: 33.5rem;
+  border-bottom: 1px solid #ccc;
+`;
+
+export const SelectorWrapper = styled.div`
+  text-align: center;
+  margin-top: 2rem;
+  display: flex;
+  width: 33.2rem;
+  border-radius: 1rem;
+  height: 4rem;
+  align-items: center;
+`;
+
+export const PostsTitle = styled.div`
+  font-size: 2rem;
+  font-weight: bold;
+`;
