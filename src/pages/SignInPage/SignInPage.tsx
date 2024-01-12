f9a95ce44548038a0eb1fd4d25022704f785c4dd
하송희 <99heesong@naver.com>
2024-01-12 11:25:11 +0900

    [✨feat]: 하단 계정 만들기 버튼 추가

--- src/pages/SignInPage/SignInPage.tsx
+++ src/pages/SignInPage/SignInPage.tsx
@@ -1,9 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+
 import SignInForm from '@/components/SignInForm/SignInForm';
 
+import { GuideComment, SignInPageContainer, SignUpButton } from './style';
+
 export default function SignInPage() {
+  const navigate = useNavigate();
+
+  const navigateToSignUpPage = () => {
+    navigate('/signUp');
+  };
+
+  const handleCreateAccount = () => {
+    navigateToSignUpPage();
+  };
+
   return (
-    <div>
+    <SignInPageContainer>
       <SignInForm />
-    </div>
+      <GuideComment>처음이신가요?</GuideComment>
+      <SignUpButton onClick={handleCreateAccount}>계정 만들기</SignUpButton>
+    </SignInPageContainer>
   );
 }
---
+++ src/pages/SignInPage/style.ts
@@ -0,0 +1,7 @@
+import styled from '@emotion/styled';
+
+export const SignInPageContainer = styled.div``;
+
+export const GuideComment = styled.span``;
+
+export const SignUpButton = styled.button``;
