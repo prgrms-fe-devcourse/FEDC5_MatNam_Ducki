1671d1bb44a18d30d62377f52bec5352bffe2453
Kyungbin <pkb8839@naver.com>
2024-01-12 14:39:14 +0900

    [✨feat] : 프로필페이지 배경 이미치 추가

--- src/components/PostUserProfile/style.ts
+++ src/components/PostUserProfile/style.ts
@@ -14,7 +14,7 @@ export const ProfileTopWrapper = styled.div`
 export const ProfileBackGroundImage = styled.div`
   width: 37.5rem;
   height: 23.2rem;
-  background-image: url('../../../public/Group.png');
+  background-image: url('../../../public/profileBackground.png');
   background-size: cover;
 `;
 
