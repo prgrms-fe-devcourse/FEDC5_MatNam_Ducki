51ea1e48ca36c22955380035a0a6c25af2eee83f
Baek <woonn97@gmail.com>
2024-01-12 13:56:58 +0900

    [✨feat]: 최신 순, 인기 순 정렬하는 기능 추가

--- src/pages/ReviewList/index.tsx
+++ src/pages/ReviewList/index.tsx
@@ -1,15 +1,33 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 import { ChannelList } from '@/components/Channel/ChannelList';
 import { ReviewCardList } from '@/components/ReviewCardList';
 import { useGetPostByChannel } from '@/hooks/usePost';
+import { Post } from '@/types/response';
+
+import { ReviewListContainer } from './style';
 
 export default function ReviewList() {
   const [channelId, setChannelId] = useState('65965f97f759661f3e669793');
+  const [sortType, setSortType] = useState('latest'); // ['latest', 'popular']
+  const [sortedPosts, setSortedPosts] = useState<Post[]>([]);
 
   const { data: postsByChannel } = useGetPostByChannel({
     channelId: channelId,
   });
+
+  useEffect(() => {
+    if (postsByChannel) {
+      if (sortType === 'popular') {
+        const popularPosts = postsByChannel.slice().sort((a, b) => {
+          return b.likes.length - a.likes.length;
+        });
+        setSortedPosts(popularPosts);
+      } else {
+        setSortedPosts(postsByChannel);
+      }
+    }
+  }, [postsByChannel, sortType]);
 
   const handleChannelId = useCallback((channelId: string) => {
     setChannelId(channelId);
@@ -17,8 +35,12 @@ export default function ReviewList() {
 
   return (
     <>
-      <ChannelList channelId={channelId} handleClick={handleChannelId} />
-      <ReviewCardList posts={postsByChannel} />
+      <ReviewListContainer>
+        <button onClick={() => setSortType('latest')}>최신 순</button>
+        <button onClick={() => setSortType('popular')}>인기 순</button>
+        <ChannelList channelId={channelId} handleClick={handleChannelId} />
+        <ReviewCardList posts={sortedPosts} />
+      </ReviewListContainer>
     </>
   );
 }
---
+++ src/pages/ReviewList/style.ts
@@ -0,0 +1,5 @@
+import styled from '@emotion/styled';
+
+export const ReviewListContainer = styled.div`
+  padding: 2rem;
+`;
