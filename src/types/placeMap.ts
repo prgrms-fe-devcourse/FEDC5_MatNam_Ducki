cd02013ed5acd795030ab06e53ded95ea8f26533
pySoo <soopy368@gmail.com>
2024-01-13 21:12:25 +0900

    [✨feat]: 지도 관련 type 선언

---
+++ src/types/placeMap.ts
@@ -0,0 +1,15 @@
+export interface PlaceType {
+  id: string;
+  place_name: string;
+  road_address_name: string;
+  address_name: string;
+  phone: string;
+  place_url: string;
+  x: string;
+  y: string;
+}
+
+export interface PositionType {
+  La: number;
+  Ma: number;
+}
