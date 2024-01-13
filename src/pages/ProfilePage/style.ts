0e4362f8916f0f80483db724745afb2a612256ed
Kyungbin <pkb8839@naver.com>
2024-01-13 14:36:53 +0900

    [♻️refactor] : PostUserProfile 추가하여 재구현

---
+++ src/pages/ProfilePage/style.ts
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+
+export const ProfileWrapper = styled.div`
+  width: 37.5rem;
+  height: 81.2rem;
+  background-color: #ffffff;
+`;
+
+export const ProfileTopWrapper = styled.div`
+  width: 100%;
+  height: 26rem;
+`;
+
+export const ProfileBackGroundImage = styled.div`
+  width: 37.5rem;
+  height: 23.2rem;
+  background-image: url('../../public/images/profileBackground.png');
+  background-size: cover;
+`;
+
+export const UserInfoWrapper = styled.div`
+  display: flex;
+  padding-top: 16rem;
+  width: 100%;
+`;
+
+export const UserWrapper = styled.div`
+  display: flex;
+  height: 30rem;
+  width: 33.5rem;
+  flex-direction: column;
+  margin: 3rem 2rem 0 2rem;
+`;
+
+export const ImageWrapper = styled.div`
+  margin-top: 1.2rem;
+  margin-left: 4rem;
+`;
+
+export const Label = styled.span`
+  color: #868686;
+  font-size: 1.3rem;
+  margin-bottom: 1rem;
+`;
