d56024886622f191646c5b07dce2ba1f91ba3ee0
Kyungbin <pkb8839@naver.com>
2024-01-14 00:38:00 +0900

    [🐛fix] : BottomNavBar 경로 오류 해결

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,7 +2,7 @@ import { Suspense } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
 
-import BottomNavBar from '@/components/BottomNavBar/BottomNavBar';
+import BottomNavBar from '@/components/BottomNavBar';
 import ErrorFallback from '@/components/Common/ErrorFallback';
 import ModalContainer from '@/components/Common/Modal/ModalContainer';
 
