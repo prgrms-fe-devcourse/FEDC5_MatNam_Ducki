1191922c9d779c38b894c64ce47193e44def32e2
Baek <woonn97@gmail.com>
2024-01-14 17:01:21 +0900

    [✨feat]: useUpdatePost 훅 추가

--- src/hooks/useCreatePost.ts
+++ src/hooks/useCreatePost.ts
@@ -1,7 +1,11 @@
 import { useMutation } from '@tanstack/react-query';
 
-import { createPost } from '@/services/Post/post';
+import { createPost, updatePost } from '@/services/Post/post';
 
 export const useCreatePost = () => {
   return useMutation({ mutationFn: createPost });
+};
+
+export const useUpdatePost = () => {
+  return useMutation({ mutationFn: updatePost });
 };
