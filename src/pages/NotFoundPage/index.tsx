747f139d37ea4a36b3a2e266a9fc8425660538fa
하송희 <99heesong@naver.com>
2024-01-15 11:54:36 +0900

    [✨feat]: 하단 버튼 클릭시 메인 페이지로 이동하는 기능 구현

--- src/pages/NotFoundPage/index.tsx
+++ src/pages/NotFoundPage/index.tsx
@@ -1,12 +1,24 @@
+import { useNavigate } from 'react-router-dom';
+
 import CTAButton from '@/components/Common/Button/CTAButton';
 
 import { NotFoundImg, NotFoundPageContainer } from './style';
 
 export default function NotFoundPage() {
+  const navigate = useNavigate();
+
+  const navigateToMainPage = () => {
+    navigate('/');
+  };
+
+  const handleNavigate = () => {
+    navigateToMainPage();
+  };
+
   return (
     <NotFoundPageContainer>
       <NotFoundImg src="/images/notFoundPageImg.png" />
-      <CTAButton>메인 페이지로 돌아가기</CTAButton>
+      <CTAButton onClick={handleNavigate}>메인 페이지로 돌아가기</CTAButton>
     </NotFoundPageContainer>
   );
 }
