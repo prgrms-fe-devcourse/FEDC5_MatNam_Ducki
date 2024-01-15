76b50abb41e5b40c5790b1fe94966ab2cda31d57
pySoo <soopy368@gmail.com>
2024-01-16 03:00:22 +0900

    [✨feat]: file 로직 제거, selectedFile이 변경되면 상위 컴포넌트에 전달해주기

--- src/components/Common/ImageUpload.tsx
+++ src/components/Common/ImageUpload.tsx
@@ -1,7 +1,9 @@
 import styled from '@emotion/styled';
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { useRecoilValue } from 'recoil';
 
 import { useModal } from '@/hooks/useModal';
+import { selectedFileAtom } from '@/recoil/selectedFile';
 import { ModalType } from '@/types/modal';
 
 interface ImageUploadProps {
@@ -29,6 +31,7 @@ const UploadContainer = styled.div<{
   align-items: center;
   position: relative;
   font-size: 1rem;
+  cursor: pointer;
 `;
 
 const ImagePreview = styled.img<{ ratio?: string }>`
@@ -68,67 +71,33 @@ export default function ImageUpload({
     image || null,
   );
 
+  const selectedFile = useRecoilValue(selectedFileAtom);
+
   const { openModal } = useModal();
 
   const handleOpenModal = () => {
     openModal({ type: ModalType.CHANGE_IMAGE });
   };
 
-  const imageInputRef = useRef<HTMLInputElement>(null);
-
-  const handleImageFilesChange = (
-    event: React.ChangeEvent<HTMLInputElement>,
-  ) => {
-    if (!event.target.files || event.target.files.length === 0) {
-      return;
-    }
-
-    const file = event.target.files[0];
-    const fileURL = URL.createObjectURL(file);
-    setSelectedImage(fileURL);
-    onFileChange(file); // 선택된 파일을 외부로 전달
-  };
-
-  const handleImageRemove = () => {
-    if (selectedImage) {
-      URL.revokeObjectURL(selectedImage);
-      setSelectedImage(null);
-    }
-
-    // 'current'가 존재하는지 확인 후, input의 value를 리셋하여 같은 파일이 다시 업로드될 수 있게 합니다.
-    if (imageInputRef.current) {
-      imageInputRef.current.value = '';
-    }
-
-    onFileChange(null); // 이미지 제거 시 외부에 null을 전달
-  };
-
-  const handleImageUpdate = () => {
-    imageInputRef.current?.click();
-  };
-
   useEffect(() => {
-    setSelectedImage(image);
-  }, [image]);
+    if (selectedFile instanceof File) {
+      const fileURL = URL.createObjectURL(selectedFile);
+      setSelectedImage(fileURL);
+      onFileChange(selectedFile);
+    } else {
+      onFileChange(null);
+    }
+  }, [selectedFile]);
 
   return (
     <section>
-      <input
-        ref={imageInputRef}
-        onChange={handleImageFilesChange}
-        type="file"
-        accept="image/*"
-        style={{ display: 'none' }} // 숨김 처리
-      />
       <UploadContainer
-        onClick={handleImageUpdate}
+        onClick={handleOpenModal}
         borderRadius={borderRadius}
         width={width}
         ratio={ratio}>
         {selectedImage ? (
-          <>
-            <ImagePreview src={selectedImage} alt="이미지 미리보기" />
-          </>
+          <ImagePreview src={selectedImage} alt="이미지 미리보기" />
         ) : (
           <span>이미지 추가</span>
         )}
