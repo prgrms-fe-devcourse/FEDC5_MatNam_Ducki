9856d56b2aae4a902947d7cb861592be233fed59
pySoo <soopy368@gmail.com>
2024-01-15 13:24:15 +0900

    [✨feat]: MainPage에서 token 여부에 따라 user 정보 업데이트

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,12 +1,29 @@
-import { Suspense } from 'react';
+import { Suspense, useEffect } from 'react';
 import { ErrorBoundary } from 'react-error-boundary';
 import { Outlet } from 'react-router-dom';
+import { useSetRecoilState } from 'recoil';
 
 import BottomNavBar from '@/components/BottomNavBar/BottomNavBar';
 import ErrorFallback from '@/components/Common/ErrorFallback';
 import ModalContainer from '@/components/Common/Modal/ModalContainer';
+import { ACCESS_TOKEN_KEY } from '@/constants/api';
+import { useCheckAuthUser } from '@/hooks/useAuth';
+import { userAtom } from '@/recoil/user';
 
 export default function MainPage() {
+  const token = localStorage.getItem(ACCESS_TOKEN_KEY);
+  const setUserData = useSetRecoilState(userAtom);
+
+  const { data: authUser, refetch } = useCheckAuthUser({ isEnabled: false });
+
+  useEffect(() => {
+    refetch();
+  }, [token]);
+
+  useEffect(() => {
+    setUserData(authUser ?? null);
+  }, [authUser]);
+
   return (
     <>
       <ErrorBoundary FallbackComponent={ErrorFallback}>
