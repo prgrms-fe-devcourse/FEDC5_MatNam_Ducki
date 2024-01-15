56f50bbe55f959e4e3958070d92ed8e4ad73b919
pySoo <soopy368@gmail.com>
2024-01-15 16:38:25 +0900

    [✨feat]: keyword에 따라 refetch하도록 수정, ReviewList 사용

--- src/pages/SearchPostPage/index.tsx
+++ src/pages/SearchPostPage/index.tsx
@@ -1,33 +1,49 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 import SearchBar from '@/components/SearchBar';
-import { SearchResultType } from '@/types/response';
 
-import ReviewList from '../ReviewList';
 import {
-  PopularKeyword,
   SearchKeyword,
   SearchKeywordWrapper,
   SearchPostPageWrapper,
 } from './style';
 import PopularSearch from '@/components/PopularSearch';
+import { ReviewCardList } from '@/components/ReviewCardList';
+import EmptySearchResult from '@/components/Common/EmptySearchResult';
+import { useSearchAll } from '@/hooks/useSearch';
 
 export default function SearchPostPage() {
   const [searchKeyword, setSearchKeyword] = useState('');
-  const [searchResult, setSearchResult] = useState<SearchResultType>(null);
+
+  const { data, refetch, isLoading } = useSearchAll(searchKeyword);
+
+  const postResult = data?.postData ?? [];
+
+  useEffect(() => {
+    if (searchKeyword) {
+      refetch();
+    }
+  }, [searchKeyword]);
 
   return (
     <SearchPostPageWrapper>
-      <SearchBar
-        onSearchKeyword={setSearchKeyword}
-        onSearchResult={setSearchResult}
-      />
-      {searchResult ? (
+      <SearchBar onSearchKeyword={setSearchKeyword} />
+      {searchKeyword ? (
         <>
           <SearchKeywordWrapper>
             <SearchKeyword>{searchKeyword} 검색 결과</SearchKeyword>
           </SearchKeywordWrapper>
-          <ReviewList />
+          {isLoading ? (
+            <span>로딩 중..</span>
+          ) : (
+            <>
+              {postResult.length !== 0 ? (
+                <ReviewCardList posts={postResult} />
+              ) : (
+                <EmptySearchResult />
+              )}
+            </>
+          )}
         </>
       ) : (
         <PopularSearch />
