a596e3029f7ddb5dcf7b5aa3c7895a6b728e0c13
pySoo <soopy368@gmail.com>
2024-01-16 02:59:24 +0900

    [✨feat]: ChangeImageModal에 file 관련 로직 추가, 전역 상태 selectedFile 업데이트

--- src/components/Common/Modal/ChangeImageModal/index.tsx
+++ src/components/Common/Modal/ChangeImageModal/index.tsx
@@ -1,14 +1,58 @@
-import { useModal } from '@/hooks/useModal';
-import { ModalType } from '@/types/modal';
+import { useRef } from 'react';
+import { useSetRecoilState } from 'recoil';
 
-import { ChangeImageModalWrapper, SelectItem, SelectList } from './style';
+import { DEFAULT_PROFILE_IMAGE } from '@/constants/profile';
+import { useModal } from '@/hooks/useModal';
+import { selectedFileAtom } from '@/recoil/selectedFile';
+import { ModalType } from '@/types/modal';
+import { convertURLtoFile } from '@/utils/file';
+
+import {
+  ChangeImageModalWrapper,
+  ImageSelectInput,
+  SelectItem,
+  SelectList,
+} from './style';
 
 export default function ChangeImageModal() {
   const { closeModal } = useModal();
+  const setSelectedFile = useSetRecoilState(selectedFileAtom);
+
+  const imageInputRef = useRef<HTMLInputElement>(null);
+
+  const handleImageUpdate = () => {
+    imageInputRef.current?.click();
+  };
+
+  const handleCloseModal = () => {
+    closeModal({ type: ModalType.CHANGE_IMAGE });
+  };
+
+  const handleImageFilesChange = (
+    event: React.ChangeEvent<HTMLInputElement>,
+  ) => {
+    if (!event.target.files || event.target.files.length === 0) {
+      return;
+    }
+
+    const selectedFile = event.target.files[0];
+    setSelectedFile(selectedFile);
+    handleCloseModal();
+  };
+
+  const handleImageRemove = async () => {
+    if (imageInputRef.current) {
+      imageInputRef.current.value = '';
+    }
+
+    const defaultImageFile = await convertURLtoFile(DEFAULT_PROFILE_IMAGE);
+    setSelectedFile(defaultImageFile);
+    handleCloseModal();
+  };
 
   const selectListData = [
-    { text: '이미지 변경', onClick: () => {} },
-    { text: '기본 이미지로 변경', onClick: () => {} },
+    { text: '이미지 변경', onClick: handleImageUpdate },
+    { text: '기본 이미지로 변경', onClick: handleImageRemove },
     {
       text: '취소',
       onClick: () => {
@@ -19,6 +63,12 @@ export default function ChangeImageModal() {
 
   return (
     <ChangeImageModalWrapper>
+      <ImageSelectInput
+        ref={imageInputRef}
+        onChange={handleImageFilesChange}
+        type="file"
+        accept="image/*"
+      />
       <SelectList>
         {selectListData.map(({ text, onClick }) => (
           <SelectItem key={text} onClick={onClick}>
