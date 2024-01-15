74faeaff7f7c894cb3114da8dc2444e34a9db857
하송희 <99heesong@naver.com>
2024-01-15 11:55:18 +0900

    [🎨style]: NotFoundPageImg 이미지 및 버튼 스타일링

--- src/pages/NotFoundPage/style.ts
+++ src/pages/NotFoundPage/style.ts
@@ -1,5 +1,12 @@
 import styled from '@emotion/styled';
 
-export const NotFoundPageContainer = styled.div``;
+export const NotFoundPageContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 4rem 2rem;
+`;
 
-export const NotFoundImg = styled.img``;
+export const NotFoundImg = styled.img`
+  width: 23.6rem;
+  margin: 4rem auto;
+`;
