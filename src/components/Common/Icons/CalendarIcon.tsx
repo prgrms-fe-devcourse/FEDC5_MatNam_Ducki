296d8f9efec2c269ce90d7b8fb1c7f00afab2d08
pySoo <soopy368@gmail.com>
2024-01-16 13:43:16 +0900

    [✨feat]: svg 형식 카멜케이스로 수정

--- src/components/Common/Icons/CalendarIcon.tsx
+++ src/components/Common/Icons/CalendarIcon.tsx
@@ -10,30 +10,30 @@ export default function CalendarIcon(props: React.ComponentProps<'svg'>) {
       <path
         d="M53.1053 14.6842V7M53.1053 14.6842V22.3684M53.1053 14.6842H35.8158M7 37.7368V72.3158C7 76.5598 10.4403 80 14.6842 80H68.4737C72.7177 80 76.1579 76.5598 76.1579 72.3158V37.7368H7Z"
         stroke="currentColor"
-        stroke-width="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M7 37.7368V22.3684C7 18.1245 10.4403 14.6842 14.6842 14.6842H22.3684"
         stroke="currentColor"
-        stroke-width="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M22.3684 7V22.3684"
         stroke="currentColor"
-        stroke-width="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M76.1578 37.7368V22.3684C76.1578 18.1245 72.7175 14.6842 68.4735 14.6842H66.5525"
         stroke="currentColor"
-        stroke-width="10"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
