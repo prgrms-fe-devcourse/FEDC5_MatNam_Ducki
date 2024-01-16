12601c0e94f790d41da0eb7edc8a367478a55a12
Baek <woonn97@gmail.com>
2024-01-16 23:32:55 +0900

    [🎨style]: 글자 삭제하고 메세지 아이콘 추가

--- src/components/Common/Icons/MessageIcon.tsx
+++ src/components/Common/Icons/MessageIcon.tsx
@@ -1,8 +1,8 @@
 export default function MessageIcon() {
   return (
     <svg
-      width="20"
-      height="19"
+      width="25"
+      height="25"
       viewBox="0 0 20 19"
       fill="none"
       xmlns="http://www.w3.org/2000/svg">
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -2,3 +2,4 @@ export const MAIN_PATH = '/';
 export const REVIEW_PATH = '/review';
 export const NOTIFICATION_PATH = '/notification';
 export const PROFILE_PATH = '/profile';
+export const MESSAGE_PATH = '/message';
