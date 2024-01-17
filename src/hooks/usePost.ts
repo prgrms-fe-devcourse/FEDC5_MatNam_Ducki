92de15ab0308a8920d6c0d01f42f47d31d8914ee
Baek <woonn97@gmail.com>
2024-01-18 00:59:53 +0900

    [🐛fix]: getPost 갯수 10개로 수정

--- src/hooks/usePost.ts
+++ src/hooks/usePost.ts
@@ -3,14 +3,10 @@ import { useMutation, useQuery } from '@tanstack/react-query';
 import { createPost, getPostByChannel, updatePost } from '@/services/Post/post';
 import { GetPostByChannelPayload } from '@/types/payload';
 
-export const useGetPostByChannel = ({
-  channelId,
-  limit = 5,
-  offset = 0,
-}: GetPostByChannelPayload) => {
+export const useGetPostByChannel = ({ channelId }: GetPostByChannelPayload) => {
   return useQuery({
-    queryKey: ['postsByChannel', channelId, limit, offset],
-    queryFn: () => getPostByChannel({ channelId, limit, offset }),
+    queryKey: ['postsByChannel', channelId],
+    queryFn: () => getPostByChannel({ channelId }),
     retry: false,
   });
 };
