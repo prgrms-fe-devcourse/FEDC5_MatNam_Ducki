9b3ad50688d206140b0c0549f4ae791dde60d436
pySoo <soopy368@gmail.com>
2024-01-17 22:47:42 +0900

    [🎨style]: 자동완성 배경색 제거

--- src/styles/Global.ts
+++ src/styles/Global.ts
@@ -157,7 +157,15 @@ export const reset = css`
       system-ui,
       sans-serif;
   }
+
   input {
     background-color: transparent;
+  }
+
+  input:-webkit-autofill,
+  input:-webkit-autofill:hover,
+  input:-webkit-autofill:focus,
+  input:-webkit-autofill:active {
+    -webkit-box-shadow: 0 0 0 30px white inset !important; // 색상 없애기
   }
 `;
