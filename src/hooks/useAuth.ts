5ce8dcff6f139f3a2bbec7b23eec7a1fbf6e7908
pySoo <soopy368@gmail.com>
2024-01-17 21:12:02 +0900

    [✨feat]: API 호출 성공 시 유저 정보 업데이트

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,10 +1,13 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
+import { SetterOrUpdater } from 'recoil';
 
 import { ACCESS_TOKEN_KEY } from '@/constants/api';
 import { checkAuthUser, signIn, signOut, signUp } from '@/services/Auth/auth';
+import { User } from '@/types/response';
 
 interface AuthProps {
   onSuccess?: () => void;
+  setUserState?: SetterOrUpdater<User | null>;
   isEnabled?: boolean;
 }
 
@@ -16,16 +19,15 @@ const authKeys = {
  * @description useMutation으로 회원가입 API를 호출하는 hook입니다. 성공 시 access token을 로컬 스토리지에 저장합니다.
  * @param onSuccess - optional) 성공 시 수행할 callback 함수를 넘겨줄 때 사용합니다.
  */
-export const useSignUp = ({ onSuccess }: AuthProps = {}) => {
+export const useSignUp = ({ onSuccess, setUserState }: AuthProps = {}) => {
   return useMutation({
     mutationFn: signUp,
     onSuccess: (data) => {
       if (data?.token) {
         localStorage.setItem(ACCESS_TOKEN_KEY, data.token);
+        setUserState?.(data.user);
 
-        if (onSuccess) {
-          onSuccess();
-        }
+        onSuccess?.();
       }
     },
   });
@@ -35,16 +37,15 @@ export const useSignUp = ({ onSuccess }: AuthProps = {}) => {
  * @description useMutation으로 로그인 API를 호출하는 hook입니다. 성공 시 access token을 로컬 스토리지에 저장합니다.
  * @param onSuccess - optional) 성공 시 수행할 callback 함수를 넘겨줄 때 사용합니다.
  */
-export const useSignIn = ({ onSuccess }: AuthProps = {}) => {
+export const useSignIn = ({ onSuccess, setUserState }: AuthProps = {}) => {
   return useMutation({
     mutationFn: signIn,
     onSuccess: (data) => {
       if (data?.token) {
         localStorage.setItem(ACCESS_TOKEN_KEY, data.token);
+        setUserState?.(data.user);
 
-        if (onSuccess) {
-          onSuccess();
-        }
+        onSuccess?.();
       }
     },
   });
@@ -54,11 +55,12 @@ export const useSignIn = ({ onSuccess }: AuthProps = {}) => {
  * @description useMutation으로 로그아웃 API를 호출하는 hook입니다. 성공 시 access token을 로컬 스토리지에서 삭제합니다.
  * @param onSuccess - optional) 성공 시 수행할 callback 함수를 넘겨줄 때 사용합니다.
  */
-export const useSignOut = ({ onSuccess }: AuthProps = {}) => {
+export const useSignOut = ({ onSuccess, setUserState }: AuthProps = {}) => {
   return useMutation({
     mutationFn: signOut,
     onSuccess: () => {
       localStorage.removeItem(ACCESS_TOKEN_KEY);
+      setUserState?.(null);
 
       if (onSuccess) {
         onSuccess();
