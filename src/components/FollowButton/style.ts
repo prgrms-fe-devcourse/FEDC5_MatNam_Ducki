92aa586ce049cec6d3cfdd11d1394324d68677b8
하송희 <99heesong@naver.com>
2024-01-30 01:48:28 +0900

    [🎨style]: 글자색 white로 변경, 각 props 직접 사용

--- src/components/FollowButton/style.ts
+++ src/components/FollowButton/style.ts
@@ -3,13 +3,12 @@ import styled from '@emotion/styled';
 import { theme } from '@/styles/Theme';
 
 export const Button = styled.button<{ isMe: boolean; isFollowing: boolean }>`
-  background-color: ${(props) =>
-    props.isMe || !props.isFollowing
-      ? theme.colors.primary
-      : theme.colors.gray};
+  background-color: ${({ isMe, isFollowing }) =>
+    isMe || !isFollowing ? theme.colors.primary : theme.colors.gray};
   font-size: 1.4rem;
   border: none;
   border-radius: 1.5rem;
   padding: 0.5rem;
+  color: white;
   cursor: pointer;
 `;
